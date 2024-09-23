110614244a3ad0216aebc1e3489f1ef8a4117a2f
cjnbennett <conor.jn.bennett@gmail.com>
2024-09-23 20:35:29 +0800

    card valuation function

--- objects/one_card.py
+++ objects/one_card.py
@@ -44,6 +44,24 @@ def calculate_trick_strength_one(trick: list[Card], possible_tricks: list[list[C
 
     return probability_of_beaten
 
+def valuate(trick: list[Card], remaining_deck: list[Card]) -> float:
+    value = 1       # start off with a valuation of unity
+
+    remaining_valuations = map(Card.strength, remaining_deck)
+    # penalise high cards
+    for card in trick:
+        strength = Card.strength(card)
+        if strength < max(remaining_valuations):            # TODO: maybe bias depending on how many cards remaining can beat it
+            value *= 1 - strength / 51
+
+    # penalise cards that form combinations
+    for card in trick:
+        for valid_trick in (*get_all_valid_tricks_two(),*get_all_valid_tricks_three(),*get_all_valid_tricks_five()):
+            if card in valid_trick:
+                value *= 0.5
+
+    return value
+
 
 def one_card_trick(state: Game) -> list[Card]:
     remaining_deck: list[Card] = list(state.remaining_deck)
